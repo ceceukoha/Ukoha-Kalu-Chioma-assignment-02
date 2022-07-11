d0f786218fe3b0a5023b5682cbe5d135cca14f37
Chioma Ukoha-Kalu <74330264+ceceukoha@users.noreply.github.com>
2022-07-11 22:25:08 +0000

    Assignment2

--- src/assignment.js
+++ src/assignment.js
@@ -14,9 +14,13 @@ const assignment = {};
  * @returns number the sum of the numbers
  */
 function sumOfNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
+
+let sum = arrayOfNumbers.reduce((a, i)=>a+i, 0);
+return sum;
+  
+  
 }
-// assignment.sumOfNumbers = sumOfNumbers;
+ assignment.sumOfNumbers = sumOfNumbers;
 
 /**
  * Challenge - 2
@@ -28,9 +32,14 @@ function sumOfNumbers(arrayOfNumbers) {
  * @returns number the count of even numbers
  */
 function countEvenNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
+
+
+let even = arrayOfNumbers.filter((item)=>item%2 ==0);
+     
+  
+    return even.length;
 }
-// assignment.countEvenNumbers = countEvenNumbers;
+assignment.countEvenNumbers = countEvenNumbers;
 
 /**
  * Challenge - 3
